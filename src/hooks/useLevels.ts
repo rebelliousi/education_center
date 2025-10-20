305a149c54b6a3ddd0512c43404939bc898c296f
rebelliousi <learneryolo3@gmail.com>
2025-10-20 11:12:19 +0500

    level hook created

---
+++ src/hooks/useLevels.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "../api";
+
+// Level veri tipini tanımla
+export interface LevelType {
+  id: number;
+  name: string;
+  order: number;
+}
+
+// API'dan seviyeleri çekme fonksiyonu
+const getLevels = async (): Promise<LevelType[]> => {
+  const response = await api.get("/levels/");
+  return response.data;
+};
+
+// React Query hook'u
+export const useLevels = () => {
+  return useQuery<LevelType[]>({
+    queryKey: ["levels"],
+    queryFn: getLevels,
+  });
+};
